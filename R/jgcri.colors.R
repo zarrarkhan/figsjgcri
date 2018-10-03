25744cf430c3bc74c5169185ec423aa6d944e280
zarrar <zarrar85@gmail.com>
2018-10-03 10:57:10 -0400

    add trial R code figsjgcri.R

---
+++ R/jgcri.colors.R
@@ -0,0 +1,14 @@
+#' Colors Schemes used in the figsjgcri package
+#'
+#' @param 
+#' @keywords keyword 1, keyword 2
+#' @export
+#' @examples
+#' jgcri.colors()
+
+jgcri.colors <- function() {
+# General purpose color scheme where sequentail colors do not clash too much
+cbPalette <<- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7",
+"#333333", "#FFCC00", "#CC6600", "#006600", "#3333CC", "#CC0033", "#0099CC", "#999966")
+
+}
